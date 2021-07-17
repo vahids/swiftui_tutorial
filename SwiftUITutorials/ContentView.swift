c0c9a4e7d2a96d7709a97fa7d9f9372cd4ebdc35
Vahid Sayad <vahid.sayad@gmail.com>
2021-07-17 12:22:19 +0430

    Initial Commit

---
+++ SwiftUITutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITutorials
+//
+//  Created by Vahid Sayad on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by Vahid Sayad on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
